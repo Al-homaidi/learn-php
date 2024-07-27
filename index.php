94807336ba662a205995cb9625d201f52f150de4
Al-Homede-1 <155636135+Al-Homede-1@users.noreply.github.com>
2024-07-27 16:47:11 +0300

    learn about BOOL and INT

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 $shadow = 12 + 12;
 echo  $shadow;
 echo gettype($shadow);   
-echo 'sahdwp';
-echo 'sahdwp';
-?>     
+echo "<br>";
+var_dump((bool) '');
+echo "<br>";
+var_dump((bool) 120);
+echo "<br>";
+// echo gettype(12);
+echo (int) 15.2 + (int) 14.7 + (int) '12 sahdw' + (10.5 + 10.5);
+?>
